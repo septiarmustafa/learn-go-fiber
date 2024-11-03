2e2e2164aaf816034786e1e8f3f96c23037930d5
septiarmustafa <septiarmustafa@gmail.com>
2024-11-03 14:43:02 +0700

    get all user

--- handler/user_handler.go
+++ handler/user_handler.go
@@ -1,9 +1,18 @@
 package handler
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"learn-go-fiber/database"
+	"learn-go-fiber/model/entity"
+	"log"
 
-func UserHandlerRead(ctx *fiber.Ctx) error {
-	return ctx.JSON(fiber.Map{
-		"data": "user",
-	})
+	"github.com/gofiber/fiber/v2"
+)
+
+func GetAllUser(ctx *fiber.Ctx) error {
+	var users []entity.User
+	err := database.DB.Find(&users).Error
+	if err != nil {
+		log.Println(err)
+	}
+	return ctx.JSON(users)
 }
